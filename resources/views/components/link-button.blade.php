ef6f9df2c16cbd4834dd7ecf0c0bf18ec80161d4
g-i-kala <5star@wp.pl>
2025-04-29 19:02:04 +0200

    feat: styling the buttons

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes(['class' => 'inline-block shadow-lg py-2 px-2 font-bold transition duration-300 hover:text-primary hover:shadow-sm']) }}>{{ $slot }}</a>
+<a {{ $attributes(['class' => 'inline-block shadow-lg py-2 px-2 font-bold transition duration-300 hover:text-primary hover:shadow-sm hover:bg-bg-dark']) }}>{{ $slot }}</a>
--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,6 +1,6 @@
 
 <a {{ $attributes->merge([
-    'class' => 'inline-block py-2 px-2 shadow-sm font-bold transition duration-300 hover:text-primary hover:bg-bg-light hover:shadow-xs'
+    'class' => 'inline-block py-2 px-2 shadow-sm font-bold transition duration-300 hover:text-primary hover:bg-bg-light hover:shadow-xl'
     ]) }}>
     <span>[ </span>
     {{ $slot }}
