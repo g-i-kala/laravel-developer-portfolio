0272899d21104c164c1cbd613a54847fbb9dd0d7
g-i-kala <5star@wp.pl>
2025-04-29 09:19:17 +0200

    feat: added AOS, styled animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,7 @@ import.meta.glob([
     '../images/**'
 ]);
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
         <h2 class="text-4xl font-bold">Creative Full-Stack Developer </h2>
         
         <div class="w-full sm:w-1/2 my-4">
-          <p class="text-xl mt-4 text-secondary text-left">From syntax to style, </p>
-            <p class="text-xl mt-4 text-secondary text-center">I build modern web experiences </p>
-              <p class="text-xl mt-4 text-secondary text-right">with Laravel, PHP & WordPress.</p>
+          <p class="text-xl mt-4 text-secondary text-left" data-aos="fade-right" data-aos-delay="400">From syntax to style, </p>
+            <p class="text-xl mt-4 text-secondary text-center" data-aos="fade" data-aos-delay="600">I build modern web experiences </p>
+              <p class="text-xl mt-4 text-secondary text-right" data-aos="fade-left" data-aos-delay="800">with Laravel, PHP & WordPress.</p>
         </div>
         <div class="mt-6 flex justify-center gap-4">
             <x-link-button href="#projects" class="btn">View Projects</x-link-button>
