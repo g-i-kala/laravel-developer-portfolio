fa1053e2dcd0bba36949bde5a6b2b0b2eebb7158
g-i-kala <karocreativedesigns@gmail.com>
2025-05-05 16:50:53 +0200

    feat: add project implemented

--- database/migrations/2025_04_21_095654_create_tech_stacks_table.php
+++ database/migrations/2025_04_21_095654_create_tech_stacks_table.php
@@ -14,7 +14,6 @@ return new class () extends Migration {
         Schema::create('tech_stacks', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignIdFor(TechStackType::class)->constrained();
             $table->timestamps();
         });
     }
