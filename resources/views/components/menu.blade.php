742579cfd180e28b53890ee22759e670cbc18f1d
g-i-kala <karocreativedesigns@gmail.com>
2025-05-05 21:15:52 +0200

    fix: menu links

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes(['class' => 'flex font-bold']) }}>
-    <x-menu-item href="#about">about</x-menu-item>
-    <x-menu-item href="#skills">skills</x-menu-item>
-    <x-menu-item href="#projects">projects</x-menu-item>
-    <x-menu-item href="#contact">contact</x-menu-item>
-</div>    
+    <x-menu-item href="/#about">about</x-menu-item>
+    <x-menu-item href="/#skills">skills</x-menu-item>
+    <x-menu-item href="/#projects">projects</x-menu-item>
+    <x-menu-item href="/#contact">contact</x-menu-item>
+</div>
