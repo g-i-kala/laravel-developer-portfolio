9d3f00692694b7717ceab703281504c8b53795c3
g-i-kala <karocreativedesigns@gmail.com>
2025-05-09 11:37:26 +0200

    feat: manage skills button added

--- resources/views/components/dev-buttons.blade.php
+++ resources/views/components/dev-buttons.blade.php
@@ -1,13 +1,18 @@
 <div class="w-full text-right">
     @auth
-        <div class="flex space-x-6 font-bold">
-            <x-link-button href="/projects/show">My Projects</x-link-button>
-            <x-link-button href="/projects/create">Add a Project</x-link-button>
+        <div class="flex flex-col items-start space-x-6 space-y-4 font-bold">
+            <div>
+                <x-link-button href="/projects/show">My Projects</x-link-button>
+                <x-link-button href="/projects/create">Add a Project</x-link-button>
+                <x-link-button href="/skills/create">Manage Skills</x-link-button>
+
+            </div>
             <form method="POST" action="/logout">
                 @csrf
-                @method("DELETE")
-    
-                <button class="inline-block shadow-md py-2 px-2 font-bold transition duration-300 hover:text-primary">Log Out</button>
+                @method('DELETE')
+
+                <button class="inline-block shadow-md py-2 px-2 font-bold transition duration-300 hover:text-primary">Log
+                    Out</button>
             </form>
         </div>
     @endauth
