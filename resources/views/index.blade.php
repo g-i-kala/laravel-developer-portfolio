3fa16339feae2166c5f989515a4a6fd490d7f2c4
g-i-kala <5star@wp.pl>
2025-06-09 19:17:48 +0200

    refactor: about section extracted

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,15 +8,7 @@
 
     <x-sections.skills />
 
-    <x-sections.section id="about">
-        <h2 class="text-3xl font-bold text-center">About Me</h2>
-        <p class="text-center mt-2 text-gray-600">I’m passionate about building creative and maintainable web systems.
-        </p>
-
-        <div class="max-w-3xl mx-auto mt-10 text-gray-700 text-md">
-            <p>{{ $developer->bio ?? 'A regular country girl with a soft spot for coding.' }}</p>
-        </div>
-    </x-sections.section>
+    <x-sections.about />
 
     <x-sections.contact />
 
